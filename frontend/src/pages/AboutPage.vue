e2fb9aef666a65344d45cfa4d7523da9be32b7d9
PtrBednarik <peter.peb.8@gmail.com>
2024-01-28 16:05:05 +0100

    Vuetify Material Framework

--- frontend/src/pages/AboutPage.vue
+++ frontend/src/pages/AboutPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Hello world</h1>
+    <v-btn prepend-icon="$vuetify"
+           append-icon="$vuetify"
+           variant="text">
+      Button
+    </v-btn>
   </div>
 
 </template>
