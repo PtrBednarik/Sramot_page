9026a0f10e3129b8e330aa7159c5cd418f2e5a37
Mieris <117741780+Mieris@users.noreply.github.com>
2024-01-28 22:14:45 +0100

    button activate

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -15,7 +15,7 @@
       <div id="dropdown" class="drop-down">
 
         <a @click="show('mobile-menu')" class="mobile-hamburger" href="javascript: void(0)">
-          <img style="background-color: transparent" id="hamburger" class="hamburger" src="/hamburger.png" alt="">
+          <img style="background-color: rgba(184, 184, 184, 0.26)" id="hamburger" class="hamburger" src="/hamburger.png" alt="">
         </a>
           <ul id="mobile-menu" class="mobile-menu" style="right: -100%;">
             <li><a href="/">Domov</a></li>
@@ -32,12 +32,13 @@
 
 <script setup>
 
+
 function show(name) {
   let x = document.getElementById(name);
   let hamburger = document.getElementById("hamburger")
 
   x.style.right = ((x.style.right !== '-100%') ? '-100%' : '0');
-  hamburger.style.backgroundColor = ((hamburger.style.backgroundColor !== 'transparent') ? 'transparent' : 'rgba(0, 0, 0, 0.05)');
+  hamburger.style.backgroundColor = ((hamburger.style.backgroundColor !== 'rgba(0, 0, 0, 0.05)') ? 'rgba(0, 0, 0, 0.05)' : 'rgba(184, 184, 184, 0.26)');
 
 
   document.addEventListener("click", (evt) => {
@@ -50,7 +51,7 @@ function show(name) {
       targetEl = targetEl.parentNode;
     } while (targetEl);
     x.style.right = "-100%";
-    hamburger.style.backgroundColor = 'transparent';
+    hamburger.style.backgroundColor = 'rgba(184, 184, 184, 0.26)';
 
 
   });
@@ -61,12 +62,11 @@ function show(name) {
 
 <style scoped>
   header {
-    position: fixed;
-    top: 0;
+    position: absolute;
+    top: 1.2rem;
     left: 0;
-    width: 100vw;
     height: 60px;
-    background-color: var(--color-background-mute);
+    background-color: transparent;
     z-index: 999;
   }
 
@@ -75,46 +75,51 @@ function show(name) {
     justify-content: space-between;
     align-items: center;
     height: inherit;
-    box-shadow: rgba(0, 0, 0, 0.05) 0 6px 24px 0, rgba(0, 0, 0, 0.08) 0 0 0 1px;
     z-index: 999;
   }
 
   ul {
+    position: fixed;
+    border-radius: 25px;
+    right: 1.5rem;
     display: flex;
+    font-family: sans-serif !important;
+    font-weight: lighter;
+    align-items: center;
     flex-direction: row;
+    justify-content: center;
+    padding: 0.85rem 2rem;
+    background-color: var(--vt-c-white-mute);
+    text-align: center;
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
   }
 
   a {
-    color: var(--vt-c-text-dark-1);
-    margin-right: 23px;
+    color: var(--vt-c-text-light);
+    margin-right: 2.3rem;
     text-transform: uppercase;
     font-size: 15px;
   }
 
   a:hover, a:active {
-    color: var(--vt-c-darkBlue);
-  }
-
-  .songs {
-    background-color: var(--vt-c-blue);
-    padding: 8px 25px;
-    box-shadow: rgba(0, 0, 0, 0.05) 0 6px 24px 0, rgba(0, 0, 0, 0.08) 0 0 0 1px;
-    margin-right: 15px;
-
-  }
-
-  .songs:hover {
-    background-color: var(--vt-c-darkBlue);
-    color: var(--vt-c-white-mute);
+    color: var(--vt-c-text-dark-1);
   }
 
   li {
     list-style-type: none;
+    display: flex;
+    align-items: center;
   }
 
   h1 {
     margin-left: 25px;
     text-transform: uppercase;
+  }
+
+  .songs {
+    margin: 0;
   }
 
 
@@ -131,9 +136,10 @@ function show(name) {
 
     .hamburger {
       width: 35px;
-      position: absolute;
-      right: 12px;
-      top: 12px;
+      background-color: var(--vt-c-white-mute);
+      position: fixed;
+      right: 1rem;
+      top: 1rem;
       padding: 5px;
       box-shadow: rgba(0, 0, 0, 0.05) 0 6px 24px 0, rgba(0, 0, 0, 0.08) 0 0 0 1px;
       border-radius: 4px;
@@ -147,7 +153,11 @@ function show(name) {
     }
 
     .logo-title {
-      margin: 0 auto;
+      position: absolute;
+      width: 100%;
+      left: 0;
+      top: 0;
+      line-height: 1.1;
     }
 
     .desktop-menu {
@@ -155,15 +165,16 @@ function show(name) {
     }
 
     .mobile-menu {
-      position: absolute;
-      top: 60px;
+      position: fixed;
+      top: 0;
       width: 100%;
       flex-direction: column;
       padding: 20px;
-      background-color: var(--color-background-mute);
+      background-color: var(--vt-c-white-mute);
       box-shadow: rgba(0, 0, 0, 0.05) 0 6px 24px 0, rgba(0, 0, 0, 0.08) 0 0 0 1px;
       z-index: 500;
       transition: 0.3s;
+      border-radius: 0;
     }
 
     .mobile-menu a {
@@ -175,7 +186,7 @@ function show(name) {
       padding: 5px;
     }
 
-    .mobile-menu .songs {
+    .mobile-menu {
       padding: 8px 20px;
     }
   }
